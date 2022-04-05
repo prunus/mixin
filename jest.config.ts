d75f40a8c0d98b8ae207cd5a44155d76fc7b4f8a
Tony <tony.js@zoho.eu>
2022-04-05 20:15:15 -0300

    test: add coverage rules for tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,21 @@
-const config = {
-  preset: 'ts-jest'
+import type {Config} from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testMatch: ['**\/src\/**\/*\.spec\.ts'],
+  collectCoverageFrom: ['**/src/**'],
+  coverageDirectory: 'coverage',
+  coverageReporters: [
+    ['text', { skipFull: true }]
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
 }
 
 export default config
