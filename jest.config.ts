62d71c4b437b0a1bed73a200f0854cde88811164
Tony <tony.js@zoho.eu>
2022-04-08 16:45:22 -0300

    test(config): fix ignore coverage pattern

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,7 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testMatch: ['**\/src\/**\/*\.spec\.ts'],
   collectCoverageFrom: ['**/src/**'],
-  coveragePathIgnorePatterns: [ '**/src/index.ts' ],
+  coveragePathIgnorePatterns: [ '<rootDir>/src/index.ts' ],
   coverageDirectory: 'coverage',
   coverageReporters: [
     ['text', { skipFull: true }]
