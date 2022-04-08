59d38dc74d271a43ecced45b58740b4fe0b77e09
Tony <tony.js@zoho.eu>
2022-04-08 15:59:32 -0300

    feat: export all features

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,5 @@
-import { AnyObject, Constructor } from "./helpers/types";
-import { MixinDesign } from "./models/MixinDesign";
-import { MixinTrait } from "./models/MixinTrait";
-
 export { Mixin } from "./decorators/Mixin";
 export { Trait } from "./decorators/Trait";
 
-type TryConstructor<T extends Constructor | ((...args: any[]) => any)> =
-  T extends Constructor<infer I, infer A> | ((...args: (infer A)[]) => infer I)
-    ? new (...args: A) => I
-    : T
-
-export const mixin = <T extends ((...args: any[]) => any) | Constructor>(target: T) => {
-  const design = MixinDesign.instance(target)
-
-  const mixin = function(this: AnyObject, ...args: unknown[]) {
-    design.construct(this, ...args)
-    return this
-  }
-
-  Object.defineProperty(mixin, 'name', { value: target.name })
-
-  return (...traits: (Constructor | Function)[]): TryConstructor<T> => {
-    design.use(...traits as Constructor[])
-    return mixin as TryConstructor<T>
-  }
-}
-
-export const trait = <T extends ((...args: any[]) => any) | Constructor>(target: T) => {
-  const trait = MixinTrait.instance(target)
-
-  return (...dependencies: (Constructor | Function)[]): TryConstructor<T> => {
-    trait.registerDependency(...dependencies as Constructor[])
-    return target as TryConstructor<T>
-  }
-}
+export { mixin } from "./helpers/mixin";
+export { trait } from "./helpers/trait";
